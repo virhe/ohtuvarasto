4bddf315f879d3b3119052c49a77acc7aba0b549
tuome <tonni@tuta.io>
2023-11-03 11:34:40 +0200

    Add task 10 fix test

--- src/varasto.py
+++ src/varasto.py
@@ -30,7 +30,7 @@ class Varasto:
 
     def ota_varastosta(self, maara):
         if maara < 0:
-            return 2.0
+            return 0.0
         if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
